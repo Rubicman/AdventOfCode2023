88eb28a2b023677b818f43ce9d46a95cec89d2a7
rubicman <kubantsev@yandex.ru>
2023-12-11 23:07:55 +0300

    Solve day 10

---
+++ src/main/kotlin/day10/part2.kt
@@ -0,0 +1,66 @@
+package day10
+
+import day10.State.*
+import lines
+import kotlin.math.abs
+
+
+fun main() {
+  val map = lines().toList()
+  val cells = Array(map.size) { Array(map[0].length) { Cell() } }
+  var square = 0L
+
+  fun dfs(current: Pair<Int, Int>, parent: Pair<Int, Int>): Boolean {
+    if (current !in cells) return false
+    if (cells[current.first][current.second].state != EMPTY) return false
+    cells[current.first][current.second].distance = cells[parent.first][parent.second].distance + 1
+    cells[current.first][current.second].state = OUTSIDE
+
+    when (map[current.first][current.second]) {
+      '|' -> listOf(current.first - 1 to current.second, current.first + 1 to current.second)
+      '-' -> listOf(current.first to current.second - 1, current.first to current.second + 1)
+      'L' -> listOf(current.first - 1 to current.second, current.first to current.second + 1)
+      'J' -> listOf(current.first - 1 to current.second, current.first to current.second - 1)
+      '7' -> listOf(current.first to current.second - 1, current.first + 1 to current.second)
+      'F' -> listOf(current.first to current.second + 1, current.first + 1 to current.second)
+      '.' -> emptyList()
+      'S' -> buildList {
+        (current.first - 1 to current.second).takeIf { it in cells && map[it.first][it.second] in setOf('|', '7', 'F') }
+          ?.let { add(it) }
+        (current.first + 1 to current.second).takeIf { it in cells && map[it.first][it.second] in setOf('|', 'L', 'J') }
+          ?.let { add(it) }
+        (current.first to current.second - 1).takeIf { it in cells && map[it.first][it.second] in setOf('-', 'L', 'F') }
+          ?.let { add(it) }
+        (current.first to current.second + 1).takeIf { it in cells && map[it.first][it.second] in setOf('-', '7', 'J') }
+          ?.let { add(it) }
+      }
+
+      else -> throw IllegalArgumentException()
+    }
+      .filter { it != parent }
+      .forEach { to ->
+        if (map[to.first][to.second] == 'S' || dfs(to, current)) {
+          cells[current.first][current.second].state = LOOP
+          square += (current.first - to.first) * current.second
+          return true
+        }
+      }
+
+    return false
+  }
+
+  val group = ThreadGroup("threadGroup")
+  for (i in map.indices) {
+    for (j in map[0].indices) {
+      if (map[i][j] == 'S') {
+        Thread(group, { dfs(i to j, i to j) }, "Calculation Thread", 200000000)
+          .apply {
+            start()
+            join()
+          }
+      }
+    }
+  }
+
+  cells.flatten().filter { it.state == LOOP }.maxOf { it.distance }.let { println(abs(square) - it / 2 + 1) }
+}
